74ec77a64fc165b17e4a1e0eff6464619b6c060e
trentmarino <trentmario@hotmail.com>
2016-04-11 20:39:00 +1000

    ddd

--- get_property_names.php
+++ get_property_names.php
@@ -1,6 +1,14 @@
 <?php
 function getCategories(){
-include 'db_connect.php';
+//include 'db_connect.php';
+$url = parse_url(getenv("mysql://b9b98665377990:b375b17e@us-cdbr-iron-east-03.cleardb.net/heroku_0b5ab614720338a?reconnect=true"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
 $response = array();
     $sql = "SELECT * FROM property WHERE deleted = 0";
     $result = $conn->query($sql);
