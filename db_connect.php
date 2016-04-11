2814291ee4281a902755455f2ef76c909cebc58b
trentmarino <trentmario@hotmail.com>
2016-04-11 19:51:18 +1000

    db connetion

--- db_connect.php
+++ db_connect.php
@@ -1,12 +1,11 @@
 <?php
-
-     $servername = "localhost";
-          $username = "bob";
-          $password = "bob";
-          $dbname = "testDB";
-
+      $url=parse_url(getenv("Cmysql://b9b98665377990:b375b17e@us-cdbr-iron-east-03.cleardb.net/heroku_0b5ab614720338a?reconnect=true"));
+      $server = $url["host"];
+       $username = $url["user"];
+       $password = $url["pass"];
+       $db = substr($url["path"],1);
           // Create connection
-          $conn = new mysqli($servername, $username, $password, $dbname);
+          $conn = new mysqli($servername, $username, $password, $db);
           // Check connection
           if ($conn->connect_error) {
               die("Connection failed: " . $conn->connect_error);
