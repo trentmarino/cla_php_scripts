d75183fe17cb27db3c1e08b55cfcd7fa03cc3b9c
trentmarino <trentmario@hotmail.com>
2016-04-11 19:55:48 +1000

    bnkjnjkn

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,9 @@
 <?php
-      $url=parse_url(getenv("mysql://b9b98665377990:b375b17e@us-cdbr-iron-east-03.cleardb.net/heroku_0b5ab614720338a?reconnect=true"));
-      $server = $url["host"];
-       $username = $url["user"];
-       $password = $url["pass"];
-       $db = substr($url["path"],1);
+      $url=parse_url(getenv("mysql://:@/?reconnect=true"));
+      $server = "us-cdbr-iron-east-03.cleardb.net";
+       $username = "b9b98665377990";
+       $password = "b375b17e";
+       $db = "heroku_0b5ab614720338a";
           // Create connection
           $conn = new mysqli($servername, $username, $password, $db);
           // Check connection
