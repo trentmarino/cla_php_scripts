d5977faab6533aa33af2ccfdec6fd5dfaa121f39
trentmarino <trentmario@hotmail.com>
2016-04-11 23:35:04 +1000

    fdggdg fucking finally

--- get_property_names.php
+++ get_property_names.php
@@ -1,10 +1,7 @@
 <?php
-//include 'db_connect.php';
+include 'db_connect.php';
 $url = parse_url(getenv("mysql://b9b98665377990:b375b17e@us-cdbr-iron-east-03.cleardb.net/heroku_0b5ab614720338a?reconnect=true"));
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
+
 
 $conn = new mysqli($server, $username, $password, $db);
 $response = array();
