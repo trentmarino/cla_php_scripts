3b7cdd549cd5ab9dfee5cfd2b82b9b165fc36764
trentmarino <trentmario@hotmail.com>
2016-04-11 19:09:15 +1000

    initial

---
+++ get_property_info_based_off_selected.php
@@ -0,0 +1,28 @@
+<?php
+function getCategories(){
+include 'db_connect.php';
+$response = array();
+    $sql = "SELECT product.product_name , property.idproperty,product.idproduct
+            FROM property
+            INNER JOIN product
+            ON property.idproperty=product.idproperty WHERE product.deleted = 0";
+    $result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+             $response["product"] = array();
+        while($row = $result->fetch_assoc()) {
+             $product = array();
+             $product["idproduct"] = $row["idproduct"];
+             $product["idproperty"] = $row["idproperty"];
+             $product["product_name"] = $row["product_name"];
+             array_push($response["product"], $product);
+
+
+            }
+ print(json_encode($response));
+        }
+
+  $conn->close();
+}
+getCategories();
+?>
---
+++ get_property_names.php
@@ -0,0 +1,24 @@
+<?php
+function getCategories(){
+include 'db_connect.php';
+$response = array();
+    $sql = "SELECT * FROM property WHERE deleted = 0";
+    $result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+             $response["product"] = array();
+        while($row = $result->fetch_assoc()) {
+             $product = array();
+             $product["idproperty"] = $row["idproperty"];
+             $product["property_name"] = $row["property_name"];
+             array_push($response["product"], $product);
+
+
+            }
+ print(json_encode($response));
+        }
+
+  $conn->close();
+}
+getCategories();
+?>
