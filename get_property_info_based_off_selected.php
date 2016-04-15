5019d07dea0f33525b38e97390b45fa3857bceec
trentmarino <trentmario@hotmail.com>
2016-04-15 17:36:17 +1000

    forgot to sync

--- get_property_info_based_off_selected.php
+++ get_property_info_based_off_selected.php
@@ -3,7 +3,8 @@
 include 'db_connect.php';
 
 $response = array();
-    $sql = "SELECT product.product_name , property.idproperty,product.idproduct, product.deposit_amount_min, product_images.image_url, product_images.is_thumb
+    $sql = "SELECT product.product_name , property.idproperty,product.idproduct, product.deposit_amount_min, product_images.image_url,
+            product_images.is_thumb
             FROM property
             INNER JOIN product
             ON property.idproperty=product.idproperty
