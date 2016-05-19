fcf189ffc49fb5d201a36c7746c8efe46585846c
trentmarino <trentmario@hotmail.com>
2016-05-19 20:44:03 +1000

    made it connect to heroku

--- db_connect.php
+++ db_connect.php
@@ -9,12 +9,12 @@ $conn = new mysqli($server, $username, $password, $db);
 
 
 
-//$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-//
-//$server = $url["host"];
-//$username = $url["user"];
-//$password = $url["pass"];
-//$db = substr($url["path"], 1);
-//
-//$conn = new mysqli($server, $username, $password, $db);
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
 ?>
